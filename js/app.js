aa9f39109618ba1a0adcec7705516f96282fbbc3
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-09-25 14:02:26 +0200

    ➕ Add a function to get the community list

--- js/app.js
+++ js/app.js
@@ -5,3 +5,21 @@ const isCommunityCodeValid = () => {
     if (COMMUNITY_CODE.length != 5) return;
     getCommunityList(COMMUNITY_CODE);
 };
+
+const getCommunityList = (communityCode) => {
+    try {
+        const COMMUNITY_LIST = fetch(
+            `https://geo.api.gouv.fr/communes?codePostal=${communityCode}`
+        )
+            .then((API_COMMUNITY_RESPONSE) => API_COMMUNITY_RESPONSE.json())
+            .then((data) =>
+                data.map((community) => [community.nom, community.code])
+            )
+            .then((list) => displayCommunity(list));
+        return COMMUNITY_LIST;
+    } catch (error) {
+        console.error("Erreur lors de la requête API :\n", error);
+    }
+};
+
+const displayCommunity = () => {};
